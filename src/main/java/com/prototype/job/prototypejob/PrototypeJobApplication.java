92f6871d9b58bfcc68d6bfd255783ac9d00c1930
sathaphornTam <sathaphorn@loxbit.co.th>
2019-12-04 13:19:26 +0700

    init

---
+++ src/main/java/com/prototype/job/prototypejob/PrototypeJobApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.job.prototypejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prototype/job/prototypejob/PrototypeJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.job.prototypejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
